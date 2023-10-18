2d3e44769378617404be16a8916b7b5720698b67
Krisna <krisnaerlangga08@gmail.com>
2023-10-18 11:11:45 +0700

    add: update promising model

--- cek.py
+++ cek.py
@@ -1,1 +1,13 @@
-import mediapipe
+import cv2 as cv
+
+
+def testDevice(source):
+    cap = cv.VideoCapture(source)
+    if cap is None or not cap.isOpened():
+        print('Warning: unable to open video source: ', source)
+    else:
+        print('Masuk kok')
+
+
+testDevice(0)  # no printout
+testDevice(1)  # prints message
