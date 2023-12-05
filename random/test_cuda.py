2bef77948845dcfe707bd470d0ef319099a3df4c
Krisna <krisnaerlangga08@gmail.com>
2023-12-05 14:43:40 +0700

    add: update dataset

--- random/test_cuda.py
+++ random/test_cuda.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
-import torch
-# print(torch.cuda.is_available())
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 print(tf.config.list_physical_devices())
+# print(tf.sysconfig.get_build_info()['cuda_version'])
